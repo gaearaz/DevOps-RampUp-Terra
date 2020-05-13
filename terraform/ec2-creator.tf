4aa8265531bf06c3f5cc92ceb2d45a5bd4ba716c
Andres Zapata <azo_67@hotmail.es>
2020-05-13 15:10:40 -0500

    several fixes and improvements-rds added

--- terraform/ec2-creator.tf
+++ terraform/ec2-creator.tf
@@ -2,10 +2,11 @@
 # ----------------------------EC2s----------------------------
 # Resource: ansible-bastion
 resource "aws_instance" "ec2-bastion" {
-  ami                         = "ami-07c1207a9d40bc3bd"
-  subnet_id                   = "subnet-f4841b8f" #Subnet for us-east-2a
-  instance_type               = "t2.micro"
-  vpc_security_group_ids      = ["sg-053c77de0b13a7275"] #default-ssh-example-2
+  ami           = "${var.ami}"
+  subnet_id     = "${var.default-subnet2-list-abc[0]}"
+  instance_type = "${var.instance-type}"
+  # vpc_security_group_ids      = ["${var.security-groups[0]}"]
+  vpc_security_group_ids      = "${var.security-groups}"
   key_name                    = "${var.key-name}"
   ebs_optimized               = "false"
   source_dest_check           = "false"
@@ -26,10 +27,10 @@ data "template_file" "bastion-user-data" {
 }
 
 resource "aws_instance" "ec2-api" {
-  ami                         = "ami-07c1207a9d40bc3bd"
-  subnet_id                   = "subnet-d28b2a9f" #Subnet for us-east-2c
-  instance_type               = "t2.micro"
-  vpc_security_group_ids      = ["sg-053c77de0b13a7275"] #default-ssh-example-2
+  ami                         = "${var.ami}"
+  subnet_id                   = "${var.default-subnet2-list-abc[2]}"
+  instance_type               = "${var.instance-type}"
+  vpc_security_group_ids      = "${var.security-groups}"
   key_name                    = "${var.key-name}"
   ebs_optimized               = "false"
   source_dest_check           = "false"
@@ -48,10 +49,10 @@ resource "aws_instance" "ec2-api" {
 }
 
 resource "aws_instance" "ec2-Ui" {
-  ami                         = "ami-07c1207a9d40bc3bd"
-  subnet_id                   = "subnet-966331ff" #Subnet for us-east-2c
-  instance_type               = "t2.micro"
-  vpc_security_group_ids      = ["sg-053c77de0b13a7275"] #default-ssh-example-2
+  ami                         = "${var.ami}"
+  subnet_id                   = "${var.default-subnet2-list-abc[2]}"
+  instance_type               = "${var.instance-type}"
+  vpc_security_group_ids      = "${var.security-groups}"
   key_name                    = "${var.key-name}"
   ebs_optimized               = "false"
   source_dest_check           = "false"
@@ -68,10 +69,10 @@ resource "aws_instance" "ec2-Ui" {
 }
 
 resource "aws_instance" "ec2-auto-server" {
-  ami                         = "ami-07c1207a9d40bc3bd"
-  subnet_id                   = "subnet-966331ff" #Subnet for us-east-2c
-  instance_type               = "t2.micro"
-  vpc_security_group_ids      = ["sg-053c77de0b13a7275"] #default-ssh-example-2
+  ami                         = "${var.ami}"
+  subnet_id                   = "${var.default-subnet2-list-abc[0]}" #Subnet for us-east-2c
+  instance_type               = "${var.instance-type}"
+  vpc_security_group_ids      = "${var.security-groups}"
   key_name                    = "${var.key-name}"
   ebs_optimized               = "false"
   source_dest_check           = "false"
@@ -88,19 +89,31 @@ resource "aws_instance" "ec2-auto-server" {
   }
 }
 
-
 data "template_file" "jenkins-user-data" {
   template = "${file("${path.module}/jenkins-user-data.tpl")}"
 }
-# ----------------------------Autoscaling group----------------------------
+# ----------------------------LoadBalancer group----------------------------
+resource "aws_lb" "ec2-applb" {
+  name               = "tf-ec2-applb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${aws_security_group.server_sg.id}"]
+  subnets            = ["${var.default-subnet2-list-abc[0]}"]
 
+  enable_deletion_protection = true
+
+  tags = {
+    Name        = "tf-ec2-lb"
+    project     = "${var.proj-tag}"
+    responsible = "${var.resp-tag}"
+  }
+}
 
 resource "aws_lb_target_group" "ec2-lb-targetGroup" {
   name     = "tf-ec2-lb-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = "${var.vpc}"
-
   # vpc_id   = "${aws_vpc.main.id}"
 }
 
@@ -138,40 +151,102 @@ resource "aws_security_group" "server_sg" {
     description = "eprovisioning"
   }
 }
-#ALB A
-
-resource "aws_launch_configuration" "ui_aws_launch"{
-  name = "tf-LC-ui"
-  instance_type = "${var.instance-type}"
-  security_groups = ["${var.instance-type}"]
-  key_name = "${var.key-name}"
-
-}
-resource "aws_launch_template" "ec2-autoscaling-group" {
+# ----------------------------Autoscaling group----------------------------
+resource "aws_launch_template" "ec2-lt-autoscaling-group" {
   name_prefix   = "tfLaunchTemplate"
   image_id      = "${var.ami}"
-  instance_type = "t2.micro"
-}#falta security group 
+  instance_type = "${var.instance-type}"
+  # security_groups = ["${aws_security_group.server_sg}"]
+}
+# ----------------------------UI Scaling stage----------------------------
+resource "aws_launch_configuration" "ui_aws_launch" {
+  name            = "tf-LC-ui"
+  image_id        = "${var.ami}"
+  instance_type   = "${var.instance-type}"
+  security_groups = ["${var.instance-type}"]
+  key_name        = "${var.key-name}"
 
-# resource "aws_autoscaling_group" "ec2-autoscaling" {
-#   name               = "tf-as-group"
-#   availability_zones = ["us-east-2a"]
-#   desired_capacity   = 1
-#   max_size           = 3
-#   min_size           = 1
+}
 
-  # tag {
-  #   key = "responsible"
-  #   value = "${var.resp-tag}"
-  #   propagate_at_launch = true
-  # }
-  #   tag {
-  #   key = "project"
-  #   value = "${var.proj-tag}"
-  #   propagate_at_launch = true
-  # }
+resource "aws_autoscaling_group" "ec2-ui-autoscaling" {
+  name               = "tf-as-group-ui"
+  availability_zones = "${var.availability-zones}"
+  desired_capacity   = 1
+  max_size           = 2
+  min_size           = 1
+  # health_check_grace_period = 300
+  # health_check_type         = "ELB"
+  # launch_configuration      = "${aws_launch_configuration.ui_aws_launch.name}"
+  # force_delete              = true
+  # vpc_zone_identifier       = ["${var.vpc.id}"]
 
-# }
+  launch_template {
+    id      = "${aws_launch_template.ec2-lt-autoscaling-group.id}"
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "responsible"
+    value               = "${var.resp-tag}"
+    propagate_at_launch = true
+  }
+  tag {
+    key                 = "project"
+    value               = "${var.proj-tag}"
+    propagate_at_launch = true
+  }
+
+}
+# ----------------------------API Scaling stage----------------------------
+resource "aws_launch_configuration" "api_aws_launch-ui" {
+  name            = "tf-LC-api"
+  image_id        = "${var.ami}"
+  instance_type   = "${var.instance-type}"
+  security_groups = ["${var.instance-type}"]
+  key_name        = "${var.key-name}"
+
+}
+
+resource "aws_autoscaling_group" "ec2-autoscaling-api" {
+  name               = "tf-as-group-api"
+  availability_zones = "${var.availability-zones}"
+  desired_capacity   = 1
+  max_size           = 2
+  min_size           = 1
+  # health_check_grace_period = 300
+  # health_check_type         = "ELB"
+  # launch_configuration      = "${aws_launch_configuration.ui_aws_launch.name}"
+  # force_delete              = true
+  # vpc_zone_identifier       = ["${var.vpc.id}"]
+
+  launch_template {
+    id      = "${aws_launch_template.ec2-lt-autoscaling-group.id}"
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "responsible"
+    value               = "${var.resp-tag}"
+    propagate_at_launch = true
+  }
+  tag {
+    key                 = "project"
+    value               = "${var.proj-tag}"
+    propagate_at_launch = true
+  }
+
+}
+# ----------------------------RDS stage----------------------------
+resource "aws_db_instance" "rds-mariadb" {
+  allocated_storage = 20
+  storage_type      = "gp2"
+  engine            = "mariadb"
+  engine_version    = "10.4.8"
+  instance_class    = "db.t2.micro"
+  name              = "movie_db"
+  username          = "root"
+  password          = "applicationuser"
+}
 
 # resource "aws_eip" "ip" {
 #   vpc      = true
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,14 @@
 variable "ami" {
     type = string
-    default = "ami-0e84e211558a022c0"
+    default = "ami-07c1207a9d40bc3bd"
+}
+variable "availability-zones" {
+    type = list
+    default = ["us-east-2a", "us-east-2b", "us-east-2c"]
+}
+variable "default-subnet2-list-abc" {
+    type = list
+    default = ["subnet-966331ff", "subnet-d28b2a9f", "subnet-d28b2a9f"]
 }
 variable "instance-type" {
     type = string
@@ -18,10 +26,11 @@ variable "resp-tag" {
     type = string
     default = "azapatao"
 }
+variable "security-groups" { #First: default-ssh-example-2, Second: 
+    type = list
+    default = ["sg-053c77de0b13a7275"]
+}
 variable "vpc" {
     type = string
     default = "vpc-64b5f70d"
 }
-# variable "image_id" {
-#   type = string
-# }
